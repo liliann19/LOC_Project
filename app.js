5bad712801ff9a3d36a928e759f875387f5c4f83
Connor <connorjohnhughes@gmail.com>
2025-10-18 21:26:53 -0700

    Installed node, express, and nodemon

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3005;
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.sendFile(`${import.meta.dirname}/views/index.html`); 
+});
+
+app.listen(PORT,() =>{
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
